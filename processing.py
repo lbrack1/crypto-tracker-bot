9c4126a252befa2dce647075f58f395ad9b2275e
Leo Brack <leo.brack@hotmail.co.uk>
2017-10-09 12:24:38 +0100

    Tweet preprocessing added, strips punctuation, tweet entities and links

--- processing.py
+++ processing.py
@@ -17,7 +17,7 @@ import string
 from nltk.tokenize import word_tokenize, RegexpTokenizer
 from nltk.corpus import stopwords
 
-#Define how often the script updates the data
+#Define how often the script updates with new data
 update_time = 5 #seconds
 
 #---------------------------------------------------------------------------
@@ -25,7 +25,7 @@ update_time = 5 #seconds
 #---------------------------------------------------------------------------
 
 # This function takes the 'created_at', 'text', 'screen_name' and 'tweet_id' and stores it
-# into a MySQL database
+# in a MySQL database
 def store_data(tps, word_freq, now_time):
     db=MySQLdb.connect(host='localhost', user='leobrack', passwd='password', db='crypto_db', charset="utf8mb4")
     cursor = db.cursor()
@@ -39,17 +39,8 @@ def store_data(tps, word_freq, now_time):
         #print "Unexpected error when saving tweet to database", sys.exc_info()[0]
     #return
 
-# This function reads tweet data from tweets that in the database raw_tweets and created at between
+# This function reads tweet data from tweets in the database raw_tweets if they are created at between
 # start and end variables   
-def read_data(start,end):
-    db=MySQLdb.connect(host='localhost', user='leobrack', passwd='password', db='crypto_db', charset="utf8mb4")
-    cursor = db.cursor()
-    read_query = "SELECT created_at, text FROM raw_tweets WHERE created_at BETWEEN %s AND %s "
-    cursor.execute(read_query, (start,end))
-    data = cursor.fetchall()
-    cursor.close()
-    db.close()
-    return data
 
 def read_data_pandas(start,end):
     db=MySQLdb.connect(host='localhost', user='leobrack', passwd='password', db='crypto_db', charset="utf8mb4")
@@ -67,39 +58,61 @@ def tweets_per_sec(tweet_df, update_time):
     num_tweets = len(tweet_df.index)
     tps = update_time/num_tweets
     return tps
+    
+def strip_links(text):
+    link_regex    = re.compile('((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)', re.DOTALL)
+    links         = re.findall(link_regex, text)
+    for link in links:
+        text = text.replace(link[0], ', ')    
+    return text
+
+def strip_all_entities(text):
+    entity_prefixes = ['@','#']
+    for separator in  string.punctuation:
+        if separator not in entity_prefixes :
+            text = text.replace(separator,' ')
+    words = []
+    for word in text.split():
+        word = word.strip()
+        if word:
+            if word[0] not in entity_prefixes:
+                words.append(word)
+    return ' '.join(words)
 
 # Function to calculate words that appear most frequently  
 def word_freq(tweet_df):
 
     # Import list of stop words
     stop_words = set(stopwords.words('english'))
-    
-    # Import tokenizer that takes care of punctuation
-    tokenizer = RegexpTokenizer(r'\w+')
 
     # List to hold all words
     all_words = []
-    table = string.maketrans("","")
-    
-    
+      
     # Loop over all tweets in section
     for row in tweet_df.text:
+                
+        result = strip_all_entities(strip_links(row)) # remove links and entities
+        result1 = re.sub(r"RT+", "", result) # remove "RT"
         
-        
-        word_tokens = word_tokenize(row)
-        print word_tokens
-        
-        
-        
+        word_tokens = word_tokenize(result1)
 
+        # Removes stop words and recreates tweet
+        filtered_sentence = [w for w in word_tokens if not w in stop_words]
+        filtered_sentence = []
+        for w in word_tokens:
+            if w not in stop_words:
+                filtered_sentence.append(w)  
+    
+        # Adds every word to a list
+        for w in filtered_sentence:
+            all_words.append(w.lower())
+        
     # Process list of words
     all_words = nltk.FreqDist(all_words)
-    print(all_words.most_common(15))
+    most_common = all_words.most_common(15)
+    return most_common
     
-    
-    
-    
-
+ 
 
 #---------------------------------------------------------------------------
 # MAIN
@@ -112,9 +125,12 @@ start = datetime.date(2017,9,21)
 end = datetime.date(2017,9,22)
 
 # Get data in pandas dataframe format
-data = read_data(start,end)
+#data = read_data(start,end)
+data = read_data_pandas(start,end)
 
 # Processing 
-#tps = tweets_per_sec(tweet_df, update_time)
-word_freq(data)
+tps = tweets_per_sec(data, update_time)
+most_common = word_freq(data)
+print tps
+print most_common
 
